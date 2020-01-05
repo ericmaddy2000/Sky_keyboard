e379ffd8b2e41087f1c3fe9e98b5561eb3a2e42f
Nya <libbyduhappy@gmail.com>
2020-01-04 20:47:38 -0500

    Initial Commit

---
+++ Sky2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sky2
+//
+//  Created by nya on 1/4/20.
+//  Copyright © 2020 nya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
